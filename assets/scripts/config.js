5d298f0cb256964ba24190b1326219ece241f19e
swardfish1 <swardstromk@gmail.com>
2018-07-05 08:47:25 -0400

    got heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://glacial-reaches-36198.herokuapp.com/',
   development: 'http://localhost:4741/'
 }
 
